eda8ccf1d93b9bd4c4938fe4348063d8c6a2f3fd
Son Roy Almerol <github@snry.me>
2024-09-08 19:39:42 -0400

    add nixd and hyprls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -20,6 +20,8 @@ local servers = {
   "omnisharp",
   "templ",
   "htmx",
+  "hyprls",
+  "nixd",
 }
 
 for _, lsp in ipairs(servers) do
@@ -92,6 +94,18 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" },
         end
       end,
     })
+  end
+})
+
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+  pattern = {"*.hl", "hypr*.conf"},
+  callback = function(event)
+      print(string.format("starting hyprls for %s", vim.inspect(event)))
+      vim.lsp.start {
+          name = "hyprlang",
+          cmd = {"hyprls"},
+          root_dir = vim.fn.getcwd(),
+      }
   end
 })
 
